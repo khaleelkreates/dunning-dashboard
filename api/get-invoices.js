73599c2f55e6a88db08e5a30df44073b09f94057
khaleelkreates <akinolalateefibrahim@gmail.com>
2026-05-26 14:14:16 +0100

    Hardcode Supabase URL in all API files

--- api/get-invoices.js
+++ api/get-invoices.js
@@ -3,7 +3,7 @@ import { google } from 'googleapis'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  'https://hiltlozttngjthpudyea.supabase.co',
   process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
